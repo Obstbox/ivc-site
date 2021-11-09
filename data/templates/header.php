75e16273b3fe27b3e60ad196684e9c36aca1df5a
ivan pashchenko <obst.box@gmail.com>
2021-11-09 12:00:59 +0500

    moving staff

--- data/templates/header.php
+++
@@ -1,6 +0,0 @@
-<div class="row" style="margin-top:10px">
-  <div class="eight columns"><h4>Портал ИВЦ</h4></div>
-  <div class="two columns">&nbsp</div>
-  <div class="two columns"><a href=index.php>на главную</a></div>
-</div>
-<hr>
